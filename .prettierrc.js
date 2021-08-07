f86b746b1004db6ada83274978cdc72dd1b028ee
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-08-07 17:37:39 -0300

    style(app): adiciona configuração do editor e Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    quoteProps: 'consistent',
+    useTabs: false,
+    tabWidth: 2,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    printWidth: 120,
+    semi: true,
+};
