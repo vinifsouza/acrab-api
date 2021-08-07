eab64e038adff6444f51a8ecf4d6bc2d0202714b
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-08-07 17:39:25 -0300

    feat(app): adiciona rota de informações sobre a aplicacao

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,8 +1,20 @@
+import AuthenticationController from './controllers/AuthenticationController';
 import express from 'express';
 import indexController from './controllers/IndexController';
 
 const router = express.Router();
 
 router.get('/', indexController.index);
+
+router.post('/login', AuthenticationController.login);
+
+router.all('*', (req, res) => {
+  res.status(404).send({
+    success: false,
+    data: {
+      error: 'Not found'
+    }
+  });
+})
 
 export default router;
