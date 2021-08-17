abf004027f66808c40374b5d8d93116ce878befa
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2021-08-17 01:28:31 -0300

    refactor(routes): :hammer: instancia uma Exception no erro 404 em vez de enviar um retorno direto

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,21 +1,18 @@
 import AuthenticationController from './controllers/AuthenticationController';
 import express from 'express';
 import indexController from './controllers/IndexController';
+import Exception from '../core/exceptions/Exception';
 
 const router = express.Router();
 
-router.post('/login', AuthenticationController.login);
 router.get('/', indexController.index);
+
+router.post('/login', AuthenticationController.login);
 
 router.use(AuthenticationController.verify);
 
-router.all('*', (req, res) => {
-  res.status(404).send({
-    success: false,
-    data: {
-      error: 'Not found',
-    },
-  });
+router.all('*', () => {
+  throw new Exception(404, 'Page not found');
 });
 
 export default router;
