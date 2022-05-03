cb8a14144be69b8a4c85a5e6da5dacf0063e8562
Vinícius Souza <39967235+vinifsouza@users.noreply.github.com>
2022-05-03 13:50:28 -0300

    ci(config): fix api port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const ACRAB_API_PORT = process.env.API_PORT || 5000;
+export const ACRAB_API_PORT = process.env.PORT || 5000;
 export const ACRAB_API_VERSION = process.env.API_VERSION || '1.0.0';
 export const ACRAB_API_SECRET = process.env.API_SECRET;
 
